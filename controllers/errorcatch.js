5e50f4021c2b979970b829abb087386ca6fab5c6
berezinke <berezinke@yandex.ru>
2022-10-21 15:08:49 +0300

    V 1.0

---
+++ controllers/errorcatch.js
@@ -0,0 +1,31 @@
+module.exports.errorCatch = (res, err) => {
+  const arrNameErrores = [
+    ['ValidationError', 'ошибка валидации', 400],
+    ['CastError', 'ошибка поиска. Инфа не найдена', 400],
+    ['DocumentNotFoundError', 'ошибка поиска. Таких данных нет', 404],
+  ];
+  const nameError = err.name;
+
+  function isInArray(element) {
+    let findResult = false;
+
+    if (element[0] === nameError) {
+      findResult = true;
+    }
+    return findResult;
+  }
+
+  const isValidError = arrNameErrores.find(isInArray);
+
+  if (isValidError) {
+    res.status(isValidError[2]).send({ message: `Произошла ${isValidError[1]}` });
+  } else {
+    console.dir(nameError);
+    console.dir(err);
+    res.status(500).send({ message: `А эту ошибку ${nameError} программа пока не знает` });
+  }
+};
+
+module.exports.errorPath = (req, res) => {
+  res.status(404).send({ message: 'Такого пути программа пока не знает' });
+};
