2a06226112adcee5d6a55ce987631d5fe759861f
berezinke <berezinke@yandex.ru>
2022-10-27 18:36:30 +0300

    V 1.0

--- .eslintrc — копия.js
+++ .eslintrc — копия.js
@@ -1,19 +1,25 @@
-{
+module.exports = {
   "env": {
     "browser": true,
     "es2021": true
     },
   "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
     "airbnb-base"
   ],
   "overrides": [
   ],
+  "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaVersion": "latest",
     "sourceType": "module"
   },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
   "rules": {
-    "underscore": 0,
-    "no-underscore-dangle":  ["error", { "allow": ["_id"] }]
+    underscore: 0
   }
 }
