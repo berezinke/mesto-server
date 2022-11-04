7f41ea55af5c55a7ffea84f1d7c38f8ba2410af2
berezinke <berezinke@yandex.ru>
2022-11-04 16:05:11 +0300

    V 1.0

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(validUrl),
-    email: Joi.string().email(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
   }),
 }), require('./controllers/auth').createUser);
 
